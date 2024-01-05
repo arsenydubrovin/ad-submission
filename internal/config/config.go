a48c7e0d00a7efc4cb50e77319264492f88823ac
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-05 22:54:44 +0300

    Change env variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,8 +7,11 @@ import (
 )
 
 type Config struct {
-	Port        string `env:"HTTP_PORT" env-default:"8080"`
-	DatabaseURL string `env:"DATABASE_URL" env-required:"true"`
+	HTTPPort     string `env:"HTTP_PORT" env-default:"8080"`
+	PostgresHost string `env:"POSTGRES_HOST" env-default:"localhost"`
+	PostgresPort string `env:"POSTGRES_PORT" env-default:"5432"`
+	PostgresUser string `env:"POSTGRES_USER" env-default:"postgres"`
+	PostgresDB   string `env:"POSTGRES_DB" env-default:"postgres"`
 }
 
 func Load() *Config {
