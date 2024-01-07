2cf71bbe8e6ae3fe0c2474c506cf45f1f9849b33
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-07 16:07:23 +0300

    Add CreateAdvert method

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Storage struct {
-	db *sql.DB
+	DB *sql.DB
 }
 
 func New(host, port, user, dbName string) (*Storage, error) {
@@ -41,7 +41,8 @@ func New(host, port, user, dbName string) (*Storage, error) {
 	CREATE TABLE IF NOT EXISTS photo_links (
 		id SERIAL PRIMARY KEY,
 		link VARCHAR(255) NOT NULL,
-		serial_number INT NOT NULL);
+		serial_number INT NOT NULL,
+	  advert_id INT REFERENCES adverts(id) ON DELETE CASCADE);
   `)
 	if err != nil {
 		return nil, err
@@ -52,5 +53,46 @@ func New(host, port, user, dbName string) (*Storage, error) {
 		return nil, err
 	}
 
-	return &Storage{db: db}, nil
+	return &Storage{DB: db}, nil
+}
+
+func (s *Storage) CreateAdvert(title, description string, photoLinks *[]string, price int) (err error) {
+	tx, err := s.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Commit or rollback depending on result of transaction
+	// Named return value (err) is required
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		}
+		if err != nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	var advertId int
+
+	err = tx.QueryRow("INSERT INTO adverts (title, description, price) VALUES ($1, $2, $3) RETURNING id",
+		title, description, price).Scan(&advertId)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO photo_links (link, serial_number, advert_id) VALUES ($1, $2, $3)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for i, link := range *photoLinks {
+		_, err = stmt.Exec(link, i+1, advertId)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
