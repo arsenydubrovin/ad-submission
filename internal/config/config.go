112cc44b84ae148312671aa1ecffa6be2cd04daa
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-08 22:32:36 +0300

    Fix config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,12 +7,16 @@ import (
 )
 
 type Config struct {
-	AppEnv       string `env:"APP_ENV" env-default:"prod"`
-	HTTPPort     string `env:"HTTP_PORT" env-default:"8080"`
-	PostgresHost string `env:"POSTGRES_HOST" env-default:"localhost"`
-	PostgresPort string `env:"POSTGRES_PORT" env-default:"5432"`
-	PostgresUser string `env:"POSTGRES_USER" env-default:"postgres"`
-	PostgresDB   string `env:"POSTGRES_DB" env-default:"postgres"`
+	App struct {
+		Env      string `env:"APP_ENV" env-default:"prod"` // default prod in more secure
+		HTTPPort string `env:"HTTP_PORT" env-default:"8080"`
+	}
+	Postgres struct {
+		Host string `env:"POSTGRES_HOST" env-default:"localhost"`
+		Port string `env:"POSTGRES_PORT" env-default:"5432"`
+		User string `env:"POSTGRES_USER" env-default:"postgres"`
+		DB   string `env:"POSTGRES_DB" env-default:"postgres"`
+	}
 }
 
 func Load() *Config {
