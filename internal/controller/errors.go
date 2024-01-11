84ddd459a1ca2c132beb480dbe4c143402a35ff7
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-11 18:59:15 +0300

    Move error responses to errors.go

---
+++ internal/controller/errors.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"log/slog"
+	"net/http"
+
+	echo "github.com/labstack/echo/v4"
+)
+
+func (c *Controller) errorResponse(ctx echo.Context, status int, mgs any) error {
+	return ctx.JSON(status, envelope{"error": mgs})
+}
+
+func (c *Controller) serverErrorResponse(ctx echo.Context, err error) error {
+	slog.Error("internal server error", slog.Any("error", err))
+
+	msg := "the server has encountered an error and cannot process the request"
+	return c.errorResponse(ctx, http.StatusInternalServerError, msg)
+}
+
+func (c *Controller) badRequestResponse(ctx echo.Context, err error) error {
+	return c.errorResponse(ctx, http.StatusBadRequest, err)
+}
+
+func (c *Controller) notFoundResponse(ctx echo.Context) error {
+	msg := "the requested resource was not found"
+	return c.errorResponse(ctx, http.StatusNotFound, msg)
+}
+
+func (c *Controller) failedValidationResponse(ctx echo.Context, errors map[string]string) error {
+	return c.errorResponse(ctx, http.StatusUnprocessableEntity, errors)
+}
